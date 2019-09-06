aabe7c5f5120572e834f6eaa91217112da52dbd0
bitromortac <bitromortac@protonmail.com>
2019-09-06 07:41:27 +0200

    add tests for ln_utilities

---
+++ test/test_ln_utilities.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from lib.ln_utilities import channel_unbalancedness_and_commit_fee
+
+
+class LnUtilityTest(TestCase):
+    def test_unbalancedness_formula(self):
+        self.assertAlmostEqual(
+            channel_unbalancedness_and_commit_fee(
+                500000, 1000000, 1000, False)[0], 0.0)
+        self.assertAlmostEqual(
+            channel_unbalancedness_and_commit_fee(
+                500000, 1000000, 1000, True)[0], -0.002)
+        self.assertAlmostEqual(
+            channel_unbalancedness_and_commit_fee(
+                600000, 1000000, 0, False)[0], -0.2)
