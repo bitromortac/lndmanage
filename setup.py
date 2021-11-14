1a2b4d1952749f09eab7541a7cac25e9f8d5a4b9
bitromortac <bitromortac@protonmail.com>
2021-11-14 11:16:16 +0100

    setup: losen requirements
    Newest versions will be installed automatically, setup.py tracks
    versions that we tested with.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re, setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('./lndmanage/__init__.py', 'r') as f:
+with open("./lndmanage/__init__.py", "r") as f:
     MATCH_EXPR = "__version__[^'\"]+(['\"])([^'\"]+)"
     VERSION = re.search(MATCH_EXPR, f.read()).group(2)
 
@@ -19,26 +19,25 @@ setuptools.setup(
     author="bitromortac",
     author_email="bitromortac@protonmail.com",
     description="Channel management tool for lightning network daemon (LND) "
-                "operators.",
+    "operators.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bitromortac/lndmanage",
     packages=setuptools.find_packages(),
     install_requires=[
-        'cycler==0.10.0',
-        'decorator==4.4.2',
-        'googleapis-common-protos==1.52.0',
-        'grpcio==1.31.0',
-        'grpcio-tools==1.31.0',
-        'kiwisolver==1.2.0',
-        'networkx==2.4',
-        'numpy==1.19.1',
-        'protobuf==3.12.4',
-        'Pygments==2.7.4',
-        'pyparsing==2.4.7',
-        'python-dateutil==2.8.1',
-        'six==1.15.0',
+        "googleapis-common-protos>=1.52.0",
+        "grpcio>=1.31.0",
+        "grpcio-tools>=1.31.0",
+        "networkx>=2.4",
+        "numpy>=1.19.1",
+        "protobuf>=3.12.4",
+        "Pygments>=2.7.4",
     ],
+    extras_require={
+        "test": [
+            "lnregtest>=0.2.1",
+        ]
+    },
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
