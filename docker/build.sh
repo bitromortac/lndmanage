a724f7d3be82bd5c499511fcfd666f271e88202c
3np <3np>
2022-04-04 18:51:58 +0900

    docker: .dockerignore instead of rsync to omit files

--- docker/build.sh
+++ docker/build.sh
@@ -6,25 +6,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 . _settings.sh
 
-# we rsync repo sources to play well with docker cache
-echo "Staging lndmanage source code..."
-mkdir -p lndmanage/_src
-rsync -a \
-  --exclude='.git/' \
-  --exclude='.idea/' \
-  --exclude='docker/' \
-  --exclude='cache/' \
-  "$LNDMANAGE_SRC_DIR" \
-  lndmanage/_src
-
-cd lndmanage
-
 echo "Building lndmanage docker container..."
 if [[ -n "$LNDMANAGE_VERBOSE" ]]; then
   set -x
 fi
 exec docker build \
-  --build-arg LNDMANAGE_HOST_SRC_PATH=_src \
+  --build-arg LNDMANAGE_HOST_SRC_PATH="${LNDMANAGE_HOST_SRC_PATH:-.}" \
   -t lndmanage:local \
+  -f ./lndmanage/Dockerfile \
   "$@" \
-  .
+  ..
